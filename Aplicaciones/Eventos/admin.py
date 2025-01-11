9e0904a0f0ab4336dbb15984d39de0e9ac28e000
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2025-01-11 10:36:56 -0500

    Creacion de modelos

--- Aplicaciones/Eventos/admin.py
+++ Aplicaciones/Eventos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import TipoUsuario, Usuario, Biografía, Festival, Presentacion, Entrada, Venta
 
 # Register your models here.
+admin.site.register(TipoUsuario)
+admin.site.register(Usuario)
+admin.site.register(Biografía)
+admin.site.register(Festival)
+admin.site.register(Presentacion)
+admin.site.register(Entrada)
+admin.site.register(Venta)
